900c18fee2c61a6c0c45a129840f19315e325c11
Kefas <cleonard@student.ciputra.ac.id>
2022-05-10 14:18:40 +0700

    coba css

--- resources/views/kasir.blade.php
+++ resources/views/kasir.blade.php
@@ -2,12 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/kasir.css" />
     <title>Kasir</title>
 </head>
 <body>
+    <div class="container"></div>
+    <!-- Bottom Navbar -->
+    <nav class="navbar navbar-expand d-md-none d-lg-none d-xl-none fixed-bottom">
+        <ul class="navbar-nav nav-justified w-100">
+        <li class="nav-item">
+            <a href="#" class="nav-link"><img src="https://img.icons8.com/ios/25/000000/cash-register.png"/></a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Cari</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Add</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Notif</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAABdUlEQVRYhe2VTU7DMBCFP1JIFpDSNd2xbA8BEhLbEgRnqAgUjoG4A1wHyqL8Iwq9BWGBhAosPFYsREKc1BuUJ40SyW/evNieCdSoYY8AGABD4E1iCBwAvuvibeAG+MqIa+E4QWAUfwJ6QCixBYxl7QpHO3FoFG/9st4yTOy7MHAp4r0czrZwLlwYSEQ8zOE0hfNaVNSraOon5mwTbAw8ynMjh6PXHmyNFMEAtb1jsi/hs3BiFwYCVJ9rExHqzJuoy6eLj3A4kNqoPs8aRCNgxVVxDR/V5+eozkjkPabElzcs+QuoqbcJrALLqJv/AcwLZwJ82hopgkjEs7Zfx0RMzgwecGIUuEWN5Q6wKNEFjoA7g3fMjOaMLv4O9P8Q9YA94WoTlRAZxdcs8tYNE3n/jlz4pGfeL5EfS+4L6vJaY5f0zMucZQO4F42dLFKecCTPU8q11RQ4k/dSXaFHa6dMsqBLOrqtof//SxUMhKKRVNCoUeOf4xvYvW2oRfLVqAAAAABJRU5ErkJggg=="/></a>
+        </li>
+        </ul>
+    </nav>
+
     <div class="container">
 
     </div>
