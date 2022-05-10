ccf7bb69445d6b95423532a1567f89e969a0e8f7
Kefas <cleonard@student.ciputra.ac.id>
2022-05-10 15:19:19 +0700

    sudah ada icon tapi ga keluar bodok

--- resources/views/kasir.blade.php
+++ resources/views/kasir.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/31f04b09a4.js" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/kasir.css" />
@@ -18,13 +19,13 @@
             <a href="#" class="nav-link"><img src="https://img.icons8.com/ios/25/000000/cash-register.png"/></a>
         </li>
         <li class="nav-item">
-            <a href="#" class="nav-link">Cari</a>
+            <a href="#" class="nav-link"><img src="https://img.icons8.com/pastel-glyph/64/000000/box--v3.png"/></a>
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link">Add</a>
         </li>
         <li class="nav-item">
-            <a href="#" class="nav-link">Notif</a>
+            <a href="/" class="nav-link"><i class="fa-regular fa-truck-field"></i></a>
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAABdUlEQVRYhe2VTU7DMBCFP1JIFpDSNd2xbA8BEhLbEgRnqAgUjoG4A1wHyqL8Iwq9BWGBhAosPFYsREKc1BuUJ40SyW/evNieCdSoYY8AGABD4E1iCBwAvuvibeAG+MqIa+E4QWAUfwJ6QCixBYxl7QpHO3FoFG/9st4yTOy7MHAp4r0czrZwLlwYSEQ8zOE0hfNaVNSraOon5mwTbAw8ynMjh6PXHmyNFMEAtb1jsi/hs3BiFwYCVJ9rExHqzJuoy6eLj3A4kNqoPs8aRCNgxVVxDR/V5+eozkjkPabElzcs+QuoqbcJrALLqJv/AcwLZwJ82hopgkjEs7Zfx0RMzgwecGIUuEWN5Q6wKNEFjoA7g3fMjOaMLv4O9P8Q9YA94WoTlRAZxdcs8tYNE3n/jlz4pGfeL5EfS+4L6vJaY5f0zMucZQO4F42dLFKecCTPU8q11RQ4k/dSXaFHa6dMsqBLOrqtof//SxUMhKKRVNCoUeOf4xvYvW2oRfLVqAAAAABJRU5ErkJggg=="/></a>
