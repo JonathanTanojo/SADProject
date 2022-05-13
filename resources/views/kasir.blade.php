f61cca68fc51ae8f513e61ac8d5cf5574b211830
Kefas <cleonard@student.ciputra.ac.id>
2022-05-13 13:20:48 +0700

    navbar

--- resources/views/kasir.blade.php
+++ resources/views/kasir.blade.php
@@ -11,30 +11,37 @@
     <title>Kasir</title>
 </head>
 <body>
-    <div class="container"></div>
-    <!-- Bottom Navbar -->
-    <nav class="navbar navbar-expand d-md-none d-lg-none d-xl-none fixed-bottom">
-        <ul class="navbar-nav nav-justified w-100">
-        <li class="nav-item">
-            <a href="#" class="nav-link"><img src="https://img.icons8.com/ios/25/000000/cash-register.png"/></a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link"><img src="https://img.icons8.com/pastel-glyph/64/000000/box--v3.png"/></a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">Add</a>
-        </li>
-        <li class="nav-item">
-            <a href="/" class="nav-link"><i class="fa-regular fa-truck-field"></i></a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAABdUlEQVRYhe2VTU7DMBCFP1JIFpDSNd2xbA8BEhLbEgRnqAgUjoG4A1wHyqL8Iwq9BWGBhAosPFYsREKc1BuUJ40SyW/evNieCdSoYY8AGABD4E1iCBwAvuvibeAG+MqIa+E4QWAUfwJ6QCixBYxl7QpHO3FoFG/9st4yTOy7MHAp4r0czrZwLlwYSEQ8zOE0hfNaVNSraOon5mwTbAw8ynMjh6PXHmyNFMEAtb1jsi/hs3BiFwYCVJ9rExHqzJuoy6eLj3A4kNqoPs8aRCNgxVVxDR/V5+eozkjkPabElzcs+QuoqbcJrALLqJv/AcwLZwJ82hopgkjEs7Zfx0RMzgwecGIUuEWN5Q6wKNEFjoA7g3fMjOaMLv4O9P8Q9YA94WoTlRAZxdcs8tYNE3n/jlz4pGfeL5EfS+4L6vJaY5f0zMucZQO4F42dLFKecCTPU8q11RQ4k/dSXaFHa6dMsqBLOrqtof//SxUMhKKRVNCoUeOf4xvYvW2oRfLVqAAAAABJRU5ErkJggg=="/></a>
-        </li>
-        </ul>
-    </nav>
-
-    <div class="container">
-
+    <div class="col-12">
+        <!-- Bottom Navbar -->
+        <nav class="navbar col-12 fixed-bottom">
+            <ul class="navbar-nav nav-justified w-100">
+                <li class="active">
+                    <a href="/" class="nav-link">
+                        <img src="img/box.png" alt="">
+                    </a>
+                </li>
+                <li >
+                    <a href="/" class="nav-link">
+                        <img src="img/cash-report.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="nav-link">
+                        <img src="img/cashier.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="nav-link">
+                        <img src="img/truck.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="nav-link">
+                        <img src="img/user.png" alt="">
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio adipisci earum
         dignissimos perferendis quis suscipit ea, soluta impedit tempora exercitationem et eaque
