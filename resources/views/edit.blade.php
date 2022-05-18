548ed1e3bf2f6150d66d24065c5a6e50d95955a6
JonathanTanojo <74892204+JonathanTanojo@users.noreply.github.com>
2022-05-18 17:07:35 +0700

    edit

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     {{-- CSS --}}
+     <link rel="stylesheet" href="jcss.css">
+     <!-- Bootstrap -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     {{-- Font --}}
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <title>Detail Produk</title>
+</head>
+<body>
+    <div class="container">
+        <div class="backgroundcolor" style="border-radius: 10px;background-color: #F3F1FF; height: 100vh;box-shadow: 3px 4px 8px rgba(0, 0, 0, 0.25);margin-top: 15px;">
+            <div class="col-12 buttonfilteratas" style="display: flex;background: #E0E4FF; border-radius: 10px;height: 45px;">
+                <div class="detail col-6">
+                    <label>Detail Produk</label>
+                </div>
+                <div class="restok col-6 ">
+                    <label>Restok Produk</label>
+                </div>
+            </div>
+            <div class="col-12 contentbawah">
+                <form action="">
+                    @csrf
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
