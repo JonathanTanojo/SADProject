8ba3a41ba2d72b35a06454cf87d03f00bb36b6d3
JonathanTanojo <74892204+JonathanTanojo@users.noreply.github.com>
2022-06-04 22:24:50 +0700

    ui update2.0

--- resources/views/keuangan.blade.php
+++ resources/views/keuangan.blade.php
@@ -6,9 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- CSS --}}
     <link rel="stylesheet" href="keuangancss.css">
-
-    <link rel="stylesheet" href="jcss.css">
-
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     {{-- Font --}}
@@ -31,6 +28,22 @@
                 <a href="" class="detail col-6" style="text-decoration: none;color:black">
                     <label>Detail Keuangan</label>
                 </a>
+            </div>
+            <div class="col-12 buttonfilteratas" style="display: flex; margin-top:15px">
+                <div class="combbox col-4">
+                    <select name="filter_produk_kategori" id="filter_produk" >
+                        <option value="" selected disabled hidden>Minggu</option>
+                        <option value="mykgrg">Minggu</option>
+                        <option value="mknringan">Bulan</option>
+                    </select>
+                </div>
+                <div class="searchbox col-4">
+                    <input type="text" name="" value="21 Maret - 29 Maret"id="" style="font-size: 12px" readonly>
+                </div>
+                <div class="searchbox col-4">
+                    <i class="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3.624,15a8.03,8.03,0,0,0,10.619.659l5.318,5.318a1,1,0,0,0,1.414-1.414l-5.318-5.318A8.04,8.04,0,0,0,3.624,3.624,8.042,8.042,0,0,0,3.624,15Zm1.414-9.96a6.043,6.043,0,1,1-1.77,4.274A6,6,0,0,1,5.038,5.038Z"/></svg></i>
+                    <input type="text" placeholder="Search..">
+                </div>
             </div>
             <div class="kontentable col-12">
                 <table class="table align-middle">
