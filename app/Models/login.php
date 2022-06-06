631ef094e89f51f3042ffaba526f92e42f1e437a
JonathanTanojo <74892204+JonathanTanojo@users.noreply.github.com>
2022-06-06 20:08:25 +0700

    login ampuni titi

--- app/Models/login.php
+++ app/Models/login.php
@@ -2,17 +2,17 @@
 
 namespace App\Models;
 
-use DB;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class login extends Model
 {
     use HasFactory;
     public function isExist($username, $password){
         $admin = "SELECT count(*) is_exist FROM PENGGUNA WHERE USERNAME= '".$username."' AND USER_KATASANDI= '".$password."';";
-        $login = DB::select($admin);
 
+        $login = DB::select($admin);
         if($login[0]->is_exist == 1){
             return true;
         }
